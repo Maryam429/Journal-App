bdd3ad5ef74fb4c6fd5642fa563664ed9880c5e8
Maryam429 <maryam.danish674@gmail.com>
2025-04-26 22:38:28 -0500

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import JournalEntry from "src/screens/JournalEntry";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqWarVcHPoEnJL0yaBYuIXQcYvXdjFIMQ",
-  authDomain: "journaling-app-dbdb7.firebaseapp.com",
-  projectId: "journaling-app-dbdb7",
-  storageBucket: "journaling-app-dbdb7.firebasestorage.app",
-  messagingSenderId: "521009770833",
-  appId: "1:521009770833:web:a1dab43c7aa54996ae6370",
-  measurementId: "G-23B8MZL593"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 //initialize Firebase
 const app = initializeApp(firebaseConfig);
